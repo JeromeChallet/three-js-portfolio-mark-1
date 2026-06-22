44c19b41fa2da3fd8b42c033a817098959306d04
JeromeChallet <jeromepchallet@gmail.com>
2026-06-22 12:12:55 +0900

    updated projects

--- src/app/data.js
+++ src/app/data.js
@@ -18,7 +18,7 @@ Websites:
 
 export const projectsData = [
   {
-    id: 1,
+    id: 30,
     name: "Rakuten Fashion",
     description:
       "React, Redux, TypeScript modernization of customer-facing fashion pages",
@@ -26,7 +26,7 @@ export const projectsData = [
     demoLink: "https://brandavenue.rakuten.co.jp/",
   },
   {
-    id: 2,
+    id: 29,
     name: "AbbVie Analytics Dashboard",
     description:
       "Responsive pharmaceutical dashboard using React, TypeScript, Zustand and Palantir Foundry",
@@ -34,7 +34,7 @@ export const projectsData = [
     demoLink: "https://www.abbvie.com/",
   },
   {
-    id: 3,
+    id: 28,
     name: "PremiumArts Dashboard",
     description:
       "React and Redux dashboard with real-time data visualization and reporting",
@@ -42,7 +42,7 @@ export const projectsData = [
     demoLink: "https://www.premiumartsinc.com/",
   },
   {
-    id: 4,
+    id: 27,
     name: "Skidata MERN Application",
     description:
       "Full-stack React, MongoDB, Express and Node.js software solution",
@@ -50,39 +50,64 @@ export const projectsData = [
     demoLink: "https://www.skidata.com/",
   },
   {
-    id: 5,
-    name: "Aoki Metaverse School",
-    description:
-      "Vue.js front-end, testing and documentation for a virtual school platform",
+    id: 26,
+    name: "The Wild Oasis",
+    description: "React/Redux app to book a suite",
     date: "2022-09-01",
-    demoLink: "https://www.aoki-style.com/",
+    demoLink: "https://nextjs-wild-oasis-ten.vercel.app/",
   },
   {
-    id: 6,
-    name: "Ocean School Database",
+    id: 25,
+    name: "Pizza Order App",
+    description: "React/Redux app to order Pizzas",
+    date: "2022-09-01",
+    demoLink: "https://main--jerome-react-fast-pizza.netlify.app//",
+  },
+  {
+    id: 24,
+    name: "Workout Timer",
+    description: "React/Redux Workout timer App",
+    date: "2022-09-01",
+    demoLink: "https://main--jerome-react-workout.netlify.app//",
+  },
+  {
+    id: 23,
+    name: "React Quizz",
+    description: "React/Redux Quizz Game about React",
+    date: "2022-09-01",
+    demoLink: "https://jeromereactquizz.netlify.app//",
+  },
+  {
+    id: 22,
+    name: "React Weather App",
+    description: "React/Redux App for weather forecast",
+    date: "2022-09-01",
+    demoLink: "https://jerome-react-classy-weather.netlify.app//",
+  },
+  {
+    id: 21,
+    name: "React Weather App",
+    description: "React/Redux App for weather forecast",
+    date: "2022-09-01",
+    demoLink: "https://jerome-react-classy-weather.netlify.app//",
+  },
+  {
+    id: 20,
+    name: "React Bill Splitter",
+    description: "React/Redux App to split the bill among friends",
+    date: "2022-09-01",
+    demoLink: "https://react-bill-splitter-jerome.netlify.app/",
+  },
+  {
+    id: 19,
+    name: "Mapty Geolocation ",
     description:
-      "JAMstack school database with APIs, serverless functions and project management",
-    date: "2021-03-01",
-    demoLink: "#",
+      "Javascript Application to create my own version of Google Map",
+    date: "2022-09-01",
+    demoLink: "/projects/09-Mapty-Geolocation-External-Libraries/index.html",
   },
   {
-    id: 7,
-    name: "World Company",
-    description:
-      "Mobile game about sustainable development built with GameMaker",
-    date: "2017-03-01",
-    demoLink: "#",
-  },
-  {
-    id: 8,
-    name: "Phenotype",
-    description:
-      "iOS game project involving Swift, Scrum, UI/UX, art, animation and sound",
-    date: "2012-08-01",
-    demoLink: "#",
-  },
-  {
-    id: 9,
+    id: 18,
     name: "The Kojima Code",
     description:
       "Unity video game built from scratch with C# and custom pixel art",
@@ -90,81 +115,136 @@ export const projectsData = [
     demoLink: "https://simmer.io/@jrme421/the-kojima-code",
   },
   {
-    id: 10,
-    name: "WPF/MSSQL Reservation App",
-    description:
-      "Desktop app for customer reservations, ticketing and equipment rentals",
-    date: "2021-01-01",
-    demoLink: "#",
+    id: 17,
+    name: "Self Driving Car Game",
+    description: "Game using LLM for testing",
+    date: "2022-01-01",
+    demoLink: "/projects/08-ML_Self_Driving_Car-main/index.html",
   },
   {
-    id: 11,
+    id: 16,
+    name: "The Kojima Code Collector's Box",
+    description: "A handmade collector edition box I created along the game",
+    date: "2022-01-01",
+    demoLink: "https://www.youtube.com/watch?v=z3dIS1JDwPw&ab_channel=jrme421",
+  },
+  {
+    id: 15,
+    name: "The Kojima Code",
+    description:
+      "Unity video game built from scratch with C# and custom pixel art",
+    date: "2022-01-01",
+    demoLink: "https://simmer.io/@jrme421/the-kojima-code",
+  },
+  {
+    id: 14,
     name: "Golds Gym Exercise App",
     description:
       "Workout planner using exercise APIs and YouTube search integration",
     date: "2022-01-01",
-    demoLink: "https://famous-treacle-4cb914.netlify.app/",
-  },
-  {
-    id: 12,
-    name: "Samsung Shopping Site",
-    description:
-      "PHP and MySQL e-commerce demo with basket and order functionality",
-    date: "2022-01-01",
-    demoLink: "https://github.com/jrme421/Samsung-shopping-site-PHP-Mysql",
+    demoLink: "https://github.com/JeromeChallet/gold-gym",
   },
   {
     id: 13,
-    name: "Video Game Database",
-    description:
-      "Firebase and Vue 3 CRUD app for tracking played and unplayed games",
+    name: "Guess My Number",
+    description: "Game made with Javascript",
     date: "2022-01-01",
-    demoLink: "https://dapper-figolla-7dd3c5.netlify.app/",
+    demoLink: "/projects/07-Project-1-Guess-My-Number/index.html",
   },
+
   {
-    id: 14,
-    name: "Rails Friends",
-    description: "Ruby on Rails CRUD app with login and Bootstrap front-end",
-    date: "2022-01-01",
-    demoLink: "https://github.com/jrme421/railsfriends",
-  },
-  {
-    id: 15,
-    name: "School Up",
-    description: "Firebase, Vue and Vuetify school database demo",
-    date: "2022-01-01",
-    demoLink: "https://schoolup-211e2.web.app/",
-  },
-  {
-    id: 16,
+    id: 12,
     name: "Omnifood",
     description:
       "Responsive HTML, CSS and JavaScript website with Netlify form handling",
     date: "2022-01-01",
-    demoLink: "https://omnifood-jeromechallet.netlify.app/",
+    demoLink: "/projects/omnifood/index.html",
   },
   {
-    id: 17,
+    id: 11,
     name: "Trillo",
     description:
       "Responsive hotel booking layout built with SASS, Flexbox and media queries",
     date: "2022-01-01",
-    demoLink: "https://fluffy-halva-cfcc98.netlify.app/",
+    demoLink: "/projects/trillo-main/index.html",
   },
   {
-    id: 18,
+    id: 10,
     name: "Nexter",
     description:
       "Responsive property agency website built with CSS Grid and SASS",
     date: "2022-01-01",
-    demoLink: "https://spontaneous-panda-159c7d.netlify.app/",
+    demoLink: "/projects/Nexter-main/index.html",
   },
   {
-    id: 19,
+    id: 9,
     name: "Natours",
     description: "Advanced responsive CSS/SASS landing page with animations",
     date: "2022-01-01",
-    demoLink: "https://spontaneous-eclair-7cf479.netlify.app/",
+    demoLink: "/projects/natour-website-main/index.html",
+  },
+
+  {
+    id: 8,
+    name: "Pig Game",
+    description: "Game made with Javascript",
+    date: "2022-01-01",
+    demoLink: "/projects/07-Project-3-Pig-Game/index.html",
+  },
+  {
+    id: 7,
+    name: "Video Game Survey Form",
+    description:
+      "Responsive HTML, CSS and JavaScript website with Netlify form handling",
+    date: "2022-01-01",
+    demoLink: "/projects/01-Free Code Camp Survey Form/index.html",
+  },
+  {
+    id: 6,
+    name: "Tribute Page",
+    description:
+      "Responsive HTML, CSS and JavaScript tribute page to Michael Chrichton",
+    date: "2022-01-01",
+    demoLink: "/projects/02-Tribute Page/index.html",
+  },
+  {
+    id: 5,
+    name: "Technical Documentation Page",
+    description:
+      "Responsive HTML, CSS and JavaScript about Javascript Documentation",
+    date: "2022-01-01",
+    demoLink: "/projects/03-Technical Documentation Page/index.html",
+  },
+  {
+    id: 4,
+    name: "Ocean School Database",
+    description:
+      "JAMstack school database with APIs, serverless functions and project management",
+    date: "2021-03-01",
+    demoLink: "#",
+  },
+  {
+    id: 3,
+    name: "The Teacher’s Guide To Japan",
+    description: "Book I have written about being a teacher in Japan",
+    date: "2021-01-21",
+    demoLink:
+      "https://www.amazon.co.jp/-/en/Williams-Pliskin/dp/B0BSJFZCZM/ref=sr_1_1?crid=2FOCU2VQAJEP&keywords=teachers+guide+to+japan&qid=1701312381&sprefix=teachers+guide+to+japn%2Caps%2C158&sr=8-1",
+  },
+  {
+    id: 2,
+    name: "Pong Arcade Machine",
+    description: "Machine I had built after graduating in Computer Science",
+    date: "2012-011-01",
+    demoLink: "https://www.youtube.com/watch?v=jahcdtqUedY&ab_channel=jrme421",
+  },
+  {
+    id: 1,
+    name: "Phenotype",
+    description:
+      "iOS game project involving Swift, Scrum, UI/UX, art, animation and sound",
+    date: "2012-08-01",
+    demoLink: "#",
   },
 ];
 
